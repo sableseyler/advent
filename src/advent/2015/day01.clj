869e794598261dc8939142b4346eb0d8a3b51fec
Sable Seyler <sable@seyleri.us>
2025-06-02 18:42:43 -0700

    2015.01: Track basement entry using reducer

--- src/advent/2015/day01.clj
+++ src/advent/2015/day01.clj
@@ -9,3 +9,13 @@
 (defn find-floor [route]
   "Given a string of paren-based floor-routing `route`, identify the destination."
   (apply + (map count-paren route)))
+
+(defn elevator-reducer [[pos floor] step]
+  (let [pos   (inc pos)
+        floor (+ floor step)]
+    (if (= floor -1)
+      (reduced pos)
+      [pos floor])))
+
+(defn basement-entered-at [route]
+  (reduce elevator-reducer [0 0] (map count-paren route)))
