f34be5321973a719c82a70a2b8ed864c76da735e
Sable Seyler <sable@seyleri.us>
2025-06-02 19:01:12 -0700

    2015.02: slack area and present total functions

--- src/advent/2015/day02.clj
+++ src/advent/2015/day02.clj
@@ -5,3 +5,12 @@
   (+ (* 2 l w)
      (* 2 w h)
      (* 2 h l)))
+
+(defn slack-area [l w h]
+  "Take the area of the smallest side, by finding the smallest edges."
+  (apply * (take 2 (sort [l w h]))))
+
+(defn present-paper [l w h]
+  "Given a present with dimensions `l`, `w`, and `h`, how much paper to wrap it?"
+  (+ (surface-area l w h)
+     (slack-area l w h)))
