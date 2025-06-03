5d3036f0b5af369a1b4d02a6397e212497150ad9
Sable Seyler <sable@seyleri.us>
2025-06-02 19:08:42 -0700

    2015.02: parse dimension strings

--- src/advent/2015/day02.clj
+++ src/advent/2015/day02.clj
@@ -1,16 +1,21 @@
-(ns advent.2015.day02)
+(ns advent.2015.day02
+  (:require [clojure.string :as str]))
 
-(defn surface-area [l w h]
+(defn surface-area [[l w h]]
   "Find surface area from dimensions `l`, `w`, and `h`"
   (+ (* 2 l w)
      (* 2 w h)
      (* 2 h l)))
 
-(defn slack-area [l w h]
+(defn slack-area [dims]
   "Take the area of the smallest side, by finding the smallest edges."
-  (apply * (take 2 (sort [l w h]))))
+  (apply * (take 2 (sort dims))))
 
-(defn present-paper [l w h]
-  "Given a present with dimensions `l`, `w`, and `h`, how much paper to wrap it?"
-  (+ (surface-area l w h)
-     (slack-area l w h)))
+(defn present-paper [dims]
+  "Given a present with dimension seq `dims`, how much paper to wrap it?"
+  (+ (surface-area dims)
+     (slack-area dims)))
+
+(defn parse-present [dimension-string]
+  "Parse rectangular dimensions of the form `LENGTHxWIDTHxHEIGHT` to integers."
+  (map parse-long (str/split dimension-string #"x")))
