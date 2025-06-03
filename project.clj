047e5ad789047b4ec44c05e5e3c933760810ba34
Sable Seyler <sable@seyleri.us>
2025-06-02 18:16:17 -0700

    Create repository and Clojure project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject advent "0.1.0-SNAPSHOT"
+  :description "Solutions for all Advent of Code years, from 2015 to the present."
+  :url "http://github.com/seylerius/advent"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns advent.core})
