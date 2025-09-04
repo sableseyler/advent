270dbb1f0bf122f92051eb95dfbae89d2f4ea146
Sable Seyler <sable@seyleri.us>
2025-09-03 18:41:41 -0700

    2015.05: Define rules as regexes

--- src/advent/2015/day05.clj
+++ src/advent/2015/day05.clj
@@ -1,1 +1,6 @@
 (ns advent.2015.day05)
+
+(def positive-rules [#"[aeiou].*[aeiou].*[aeiou]" ;; At least three vowels
+                     #"(\p{Lower})\1"]) ;; At least one doubled letter
+
+(def negative-rules [#"ab|cd|pq|xy"]) ;; Banned strings
