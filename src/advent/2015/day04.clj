90a840ede4903777ec8885b953e39946b5375ded
Sable Seyler <sable@seyleri.us>
2025-09-03 18:23:14 -0700

    2015.04: Process inputs with a reducer

--- src/advent/2015/day04.clj
+++ src/advent/2015/day04.clj
@@ -13,3 +13,11 @@
 
 (defn valid-hash? [hash]
   (str/starts-with? hash "00000"))
+
+(defn validate-input [_ input]
+  (if (valid-hash? (digest/md5 input))
+    (reduced (subs input 6))
+    input))
+
+(defn first-valid-hash [key]
+  (reduce validate-input "" (hash-inputs key)))
