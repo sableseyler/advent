0e7a8177b02d53d4c5f9e7c02246eafd75ef0a5d
Sable Seyler <sable@seyleri.us>
2025-09-03 18:29:46 -0700

    2015.04: Allow specifying arbitrary validators

--- src/advent/2015/day04.clj
+++ src/advent/2015/day04.clj
@@ -11,13 +11,16 @@
        (map (partial conj [key]))
        (map str/join)))
 
-(defn valid-hash? [hash]
+(defn valid5-hash? [hash]
   (str/starts-with? hash "00000"))
 
-(defn validate-input [key input]
+(defn valid6-hash? [hash]
+  (str/starts-with? hash "000000"))
+
+(defn validate-input [valid-hash? key input]
   (if (valid-hash? (digest/md5 input))
     (reduced (subs input (count key)))
     key))
 
-(defn first-valid-hash [key]
-  (reduce validate-input key (hash-inputs key)))
+(defn first-valid-hash [validator key]
+  (reduce (partial validate-input validator) key (hash-inputs key)))
