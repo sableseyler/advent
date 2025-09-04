ba75b528398da22d7682ace8404664058cbe0514
Sable Seyler <sable@seyleri.us>
2025-09-03 21:15:34 -0700

    2015.06: Write integer-brightness variants

--- src/advent/2015/day06.clj
+++ src/advent/2015/day06.clj
@@ -2,12 +2,23 @@
 
 (def light-grid (repeat 1000 (repeat 1000 false)))
 
+(def int-grid (repeat 1000 (repeat 1000 0)))
+
 (defn parse-instruction [instr]
   (let [[_ action x1 y1 x2 y2] (re-matches #"(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)" instr)]
     [(case action
        "turn on"  (fn [_] true)
        "turn off" (fn [_] false)
        "toggle"   not)
+     (map parse-long [x1 x2])
+     (map parse-long [y1 y2])]))
+
+(defn parse-int-instruction [instr]
+  (let [[_ action x1 y1 x2 y2] (re-matches #"(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)" instr)]
+    [(case action
+       "turn on"  inc
+       "turn off" dec
+       "toggle"   #(+ 2 %))
      (map parse-long [x1 x2])
      (map parse-long [y1 y2])]))
 
@@ -29,13 +40,28 @@
        (map parse-instruction)
        (reduce change-lights light-grid)))
 
+(defn configure-int-lights [instructions]
+  (->> instructions
+       (map parse-int-instruction)
+       (reduce change-lights int-grid)))
+
 (defn count-lights [grid]
   (->> grid
        (map (partial filter identity))
        (map count)
        (reduce +)))
 
+(defn sum-brightness [grid]
+  (->> grid
+       (map (partial reduce +))
+       (reduce +)))
+
 (defn count-santa-lights [instructions]
   (-> instructions
       configure-lights
       count-lights))
+
+(defn sum-santa-lights [instructions]
+  (-> instructions
+      configure-int-lights
+      sum-brightness))
