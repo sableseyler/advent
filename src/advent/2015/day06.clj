7426676f06c1e06c4ba65be6dc2a08849e478abc
Sable Seyler <sable@seyleri.us>
2025-09-03 20:26:06 -0700

    2015.06: Parse instructions with regex

--- src/advent/2015/day06.clj
+++ src/advent/2015/day06.clj
@@ -1,3 +1,12 @@
 (ns advent.2015.day06)
 
 (def light-grid (repeat 1000 (repeat 1000 false)))
+
+(defn parse-instruction [instr]
+  (let [[_ action x1 y1 x2 y2] (re-matches #"(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)" instr)]
+    [(case action
+       "turn on"  (fn [_] true)
+       "turn off" (fn [_] false)
+       "toggle"   not)
+     [x1 x2]
+     [y1 y2]]))
