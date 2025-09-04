fc7fed67f1586b73faf1f1c5c3fcf864b510b3f1
Sable Seyler <sable@seyleri.us>
2025-09-03 18:07:36 -0700

    2015.04: Generate hash inputs
    Used lazy sequences and mapping to generate the hash inputs.

--- src/advent/2015/day04.clj
+++ src/advent/2015/day04.clj
@@ -1,1 +1,11 @@
-(ns advent.2015.day04)
+(ns advent.2015.day04
+  (:require [clojure.string :as str]))
+
+(def secret-key "yzbqklnj")
+
+(defn hash-inputs [key]
+  (->> (range)
+       rest
+       (map str)
+       (map (partial conj [key]))
+       (map str/join)))
