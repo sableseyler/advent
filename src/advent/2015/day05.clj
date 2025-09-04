3918836bc1de7a5dff0796aaad0bea616d1fe92d
Sable Seyler <sable@seyleri.us>
2025-09-03 19:04:49 -0700

    2015.05: Refactor rulesets as argument

--- src/advent/2015/day05.clj
+++ src/advent/2015/day05.clj
@@ -1,13 +1,18 @@
 (ns advent.2015.day05)
 
-(def positive-rules [#"[aeiou].*[aeiou].*[aeiou]" ;; At least three vowels
-                     #"(\p{Lower})\1"]) ;; At least one doubled letter
+(def ruleset1 [[#"[aeiou].*[aeiou].*[aeiou]" ;; At least three vowels
+                #"(\p{Lower})\1"] ;; At least one doubled letter
 
-(def negative-rules [#"ab|cd|pq|xy"]) ;; Banned strings
+               [#"ab|cd|pq|xy"]]) ;; Banned strings
 
-(defn nice? [s]
-  (and (every? #(re-find % s) positive-rules)
-       (not-any? #(re-find % s) negative-rules)))
+(def ruleset2 [[#"(\p{Lower}{2}).*\1" ;; Duplicate non-overlapping letter pair
+                #"(\p{Lower})\p{Lower}\1"] ;; Repeat letter with middle
 
-(defn count-niceness [strings]
-  (count (filter nice? strings)))
+               []]) ;; No negative rules
+
+(defn nice? [[positive-rules negative-rules] s]
+  (and (if (seq positive-rules) (every? #(re-find % s) positive-rules) true)
+       (if (seq negative-rules) (not-any? #(re-find % s) negative-rules) true)))
+
+(defn count-niceness [ruleset strings]
+  (count (filter (partial nice? ruleset) strings)))
